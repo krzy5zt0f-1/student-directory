36762a09160aff57b76096de3e5d25607917c726
Krzysztof Kasprzak <timmmc88@gmail.com>
2020-10-02 20:29:58 +0100

    version of dierectory.rb that uses hashes

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
-# store students in an array
+# store students in an array of hashes
 students = [
-  "Dr Hannibal Lecter",
-  "Darth vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+{name: "Dr. Hannibal Lecter", cohort: :november},
+{name: "Darth Vader", cohort: :november},
+{name: "Nurse Ratched", cohort: :november},
+{name: "Michael Corleone", cohort: :november},
+{name: "Alex DeLarge", cohort: :november},
+{name: "The Wicked Witch of the West", cohort: :november},
+{name: "Terminator", cohort: :november},
+{name: "Freddy Krueger", cohort: :november},
+{name: "The Joker", cohort: :november},
+{name: "Joffrey Baratheon", cohort: :november},
+{name: "Norman Bates", cohort: :november}
 ]
  # keep outputs as methods
 def print_header
@@ -19,7 +19,7 @@ def print_header
 end
 
 def print(names)
-  names.each { |x| puts x }
+  names.each { |x| puts "#{x[:name]} (#{x[:cohort]})" }
 end
 
 def print_footer(names)
