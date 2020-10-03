0ff2a937080e8aa6c3844a904c91ca4a5f3702e4
Krzysztof Kasprzak <timmmc88@gmail.com>
2020-10-03 13:20:18 +0100

    directory.rv witrh while loop, aphabetical filter and reduction of outputs by size of string

--- directory.rb
+++ directory.rb
@@ -18,27 +18,53 @@ def input_students
 end
 
 # keep outputs as methods
-def print_header
- puts "The students of Villains Academy"
- puts "-------------"
+#loop to print names and filter through alphabetically
+def loop_for_print(arr, control)
+  # empty bin with results form this definition to print at the end
+  bin = []
+  if control == nil
+    bin << "The students of Villains Academy"
+    bin << "-------------"
+    i = 0
+    while i != arr.size
+      bin << "#{i + 1}. #{arr[i][:name]} (#{arr[i][:cohort]} cohort)"
+      i += 1
+    end
+  else
+    bin << "The students of Villains Academy with names starting with letter `#{control}`"
+    bin << "-------------"
+    for i in 1..arr.size
+      bin << "#{i}. #{arr[i - 1][:name]} (#{arr[i - 1][:cohort]} cohort)" if arr[i - 1][:name][0] == control
+    end
+  end
+  bin
+end
+# asks for correct input for  decision on whether
+# to shorten the list to names thqt are 12 characters or shorter
+def move
+  loop do
+    @input = gets.strip
+    if @input == "y" || @input == "n"
+      break
+    end
+  end
+  @input
 end
 
 def print(names)
+
   puts "Chose the first letter of names of students to print"
   puts "To print the whole list just hit return"
   input = gets.strip.upcase
   letter = input[0]
 
-  if letter == nil
-    print_header
-    names.each.with_index(1) { |x, index| puts "#{index}. #{x[:name]} (#{x[:cohort]} cohort)" }
-  else
-    puts "The students of Villains Academy with names starting with letter `#{letter}`"
-    puts "-------------"
-    for i in 1..names.size
-      puts "#{i}. #{names[i - 1][:name]} (#{names[i - 1][:cohort]} cohort)" if names[i - 1][:name][0] == letter
-    end
-  end
+  # option to print names of students of lenghts up to 12 characters
+  puts "Would you like to view only students with names shorter than 12 characters? [y/n]"
+  input = move
+  input == "y" ? names_s = loop_for_print(names,letter).drop(2).select { |x| x.length > 33} : names_s = []
+  puts loop_for_print(names, letter) - names_s
+
+
 end
 
 def print_footer(names)
