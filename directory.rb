810bc619f8e819af7ab16e643842665e6dd2478e
Krzysztof Kasprzak <timmmc88@gmail.com>
2020-10-03 11:08:21 +0100

    directory.rb with option to filter through names startign with given letter

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
 # input_users method makes it great to eliminate hard-coded student data
 def input_students
  puts "Please enter the names of students"
- puts "To finish, justhit return twice"
+ puts "To finish, just hit return twice"
  # create an empty array
  students = []
- name = gets.chomp
+ name = gets.chomp.capitalize
  # while the name is not empty, repeat this code
  while !name.empty? do
    #add student hash to the array
    students << { name: name, cohort: "november"}
    puts "Now we have #{students.size} students"
-   name = gets.chomp
+   name = gets.chomp.capitalize
  end
  # return the array of students
  students
@@ -24,7 +24,21 @@ def print_header
 end
 
 def print(names)
- names.each.with_index(1) { |x, index| puts "#{index}. #{x[:name]} (#{x[:cohort]} cohort)" }
+  puts "Chose the first letter of names of students to print"
+  puts "To print the whole list just hit return"
+  input = gets.strip.upcase
+  letter = input[0]
+
+  if letter == nil
+    print_header
+    names.each.with_index(1) { |x, index| puts "#{index}. #{x[:name]} (#{x[:cohort]} cohort)" }
+  else
+    puts "The students of Villains Academy with names starting with letter `#{letter}`"
+    puts "-------------"
+    for i in 1..names.size
+      puts "#{i}. #{names[i - 1][:name]} (#{names[i - 1][:cohort]} cohort)" if names[i - 1][:name][0] == letter
+    end
+  end
 end
 
 def print_footer(names)
@@ -32,6 +46,5 @@ def print_footer(names)
 end
 #nothing happens until we call the methods
 students = input_students
-print_header
 print(students)
 print_footer(students)
