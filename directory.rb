da0d1f8a069d00e36ad58c488b6cb1347d0e48ad
Krzysztof Kasprzak <timmmc88@gmail.com>
2020-10-03 10:09:33 +0100

    no hard code in the directory.rb

--- directory.rb
+++ directory.rb
@@ -1,31 +1,37 @@
-# store students in an array of hashes
-students = [
-{name: "Dr. Hannibal Lecter", cohort: :november},
-{name: "Darth Vader", cohort: :november},
-{name: "Nurse Ratched", cohort: :november},
-{name: "Michael Corleone", cohort: :november},
-{name: "Alex DeLarge", cohort: :november},
-{name: "The Wicked Witch of the West", cohort: :november},
-{name: "Terminator", cohort: :november},
-{name: "Freddy Krueger", cohort: :november},
-{name: "The Joker", cohort: :november},
-{name: "Joffrey Baratheon", cohort: :november},
-{name: "Norman Bates", cohort: :november}
-]
- # keep outputs as methods
+# input_users method makes it great to eliminate hard-coded student data
+def input_students
+ puts "Please enter the names of students"
+ puts "To finish, justhit return twice"
+ # create an empty array
+ students = []
+ name = gets.chomp
+ # while the name is not empty, repeat this code
+ while !name.empty? do
+   #add student hash to the array
+   students << { name: name, cohort: "november"}
+   puts "Now we have #{students.size} students"
+   name = gets.chomp
+ end
+ # return the array of students
+ students
+
+end
+
+# keep outputs as methods
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+ puts "The students of Villains Academy"
+ puts "-------------"
 end
 
 def print(names)
-  names.each { |x| puts "#{x[:name]} (#{x[:cohort]} cohort)" }
+ names.each { |x| puts "#{x[:name]} (#{x[:cohort]} cohort)" }
 end
 
 def print_footer(names)
-  puts "Over all, we have #{names.size} great students"
+ puts "Over all, we have #{names.size} great students"
 end
 #nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
