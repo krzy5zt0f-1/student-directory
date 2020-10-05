6108764e62bd474c9013aef7feaac15fc4041f7a
Krzysztof Kasprzak <timmmc88@gmail.com>
2020-10-05 17:06:47 +0100

    directory.rb with user defined cohort inteake for each student

--- directory.rb
+++ directory.rb
@@ -1,17 +1,40 @@
 # input_users method makes it great to eliminate hard-coded student data
 def input_students
+  # months hash to define cohort intake for each student
+months = {
+  1 => "January",
+  2 => "February",
+  3 => "March",
+  4 => "April",
+  5 => "May",
+  6 => "June",
+  7 => "July",
+  8 => "August",
+  9 => "September",
+  10 => "October",
+  11 => "Novemebr",
+  12 => "Decemebr",
+}
  puts "Please enter the names of students".center(40)
  puts "To finish, just hit return twice".center(40)
- # create an empty array
+ puts " and select any arbitrarry cohort".center(40)
+ # create an empty arrays for students storage
  students = []
  name = gets.chomp.capitalize
+ # asks for the cohort of a student
+  puts "To choose the cohort".center(40)
+  puts "of the student inlucde it as an integer,".center(40)
+  puts "i.e. 1 -> January, 9 -> September.".center(40)
+  month = from1to12
  # while the name is not empty, repeat this code
  while !name.empty? do
    #add student hash to the array
-   students << { name: name, cohort: "november", hobby: "being bad",
+   students << { name: name, cohort: months[month], hobby: "being bad",
                   country_of_origin: "land of badness"}
    puts "Now we have #{students.size} students".center(40)
    name = gets.chomp.capitalize
+   puts "Specify cohort".center(40)
+   month = from1to12
  end
  # return the array of students
  students
@@ -41,7 +64,7 @@ def loop_for_print(arr, control)
   bin
 end
 # asks for correct input for  decision on whether
-# to shorten the list to names thqt are 12 characters or shorter
+# to shorten the list to names that are 12 characters or shorter
 def move
   loop do
     @input = gets.strip
@@ -51,6 +74,17 @@ def move
   end
   @input
 end
+ # askas for correct cohort input
+def from1to12
+  a = Array(1..12)
+  loop do
+    @number = gets.strip.to_i
+    if a.include?(@number)
+      break
+    end
+  end
+  @number
+end
 
 def print(names)
 
@@ -58,7 +92,6 @@ def print(names)
   puts "To print the whole list just hit return".center(40)
   input = gets.strip.upcase
   letter = input[0]
-
   # option to print names of students of lenghts up to 12 characters
   puts "Would you like to view only students with names shorter than 12 characters? [y/n]".center(40)
   input = move
