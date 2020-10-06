a6c5acdea3449403fd2f308e137f219136d9735f
Krzysztof Kasprzak <timmmc88@gmail.com>
2020-10-06 17:35:37 +0100

    refactored interactive.rb

--- interactive.rb
+++ interactive.rb
@@ -1,20 +1,18 @@
+@students = Array.new # an empty array for all methods to access
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
   # add the student hash to the array
-  students << {name: name, cohort: "november"}
-  puts "Now we have #{students.count} students"
+  @students << {name: name, cohort: "november"}
+  puts "Now we have #{@students.count} students"
   # get another name from the user
   name = gets.chomp
   end
-  # return the array of students
-  students
 end
 
 def print_header
@@ -22,36 +20,49 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each { |student| puts "#{student[:name]} (#{student[:cohort]})"}
+def print_
+  @students.each { |student| puts "#{student[:name]} (#{student[:cohort]})" }
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
-def interactive_menu
-  students = []
-  loop do
-    # 1. Print the menu and ask the user what to do
+def print_menu
+  # 1. Print the menu and ask the user what to do
     puts "1. Input the students"
     puts "2. Show the students"
     puts "9. Exit" # 9 because we'll be adding more items
-    # 2. Read the input and save into a variable
-    selection = gets.chomp
-    # 3. Do what the user has asked
-    case selection
+end
+
+def show_students
+  print_header
+  print_
+  print_footer(students)
+end
+
+def process(selection)
+  case selection
     when "1"
-      students = input_students
+      input_students
     when "2"
       print_header
-      print(students)
-      print_footer(students)
+      print_
+      print_footer
     when "9"
       exit  # this will cause the program to terminate
     else
       puts "I don't know what you meant, try again"
     end
+
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    selection = gets.chomp
+    process(selection)
   end
 end
+
 interactive_menu
