a28b36d9939dd1b3db115c529a45cd79d01abe0d
Krzysztof Kasprzak <timmmc88@gmail.com>
2020-10-06 16:52:23 +0100

    interactive.rb is an interactive, simple student-directory

---
+++ interactive.rb
@@ -0,0 +1,57 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+  # add the student hash to the array
+  students << {name: name, cohort: "november"}
+  puts "Now we have #{students.count} students"
+  # get another name from the user
+  name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+def print_header
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each { |student| puts "#{student[:name]} (#{student[:cohort]})"}
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+def interactive_menu
+  students = []
+  loop do
+    # 1. Print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    # 2. Read the input and save into a variable
+    selection = gets.chomp
+    # 3. Do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit  # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+interactive_menu
